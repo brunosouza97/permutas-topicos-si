d18b798105666ef705db65eb6569344ec8d4b8fb
brunosouza97 <bruno.souza.97@edu.unipar.br>
2018-05-29 19:44:57 -0300

    Deleção de arquivos

--- app-tesi/app/src/main/java/com/example/brunosouza/forcemusic/MainActivity.java
+++ app-tesi/app/src/main/java/com/example/brunosouza/forcemusic/MainActivity.java
@@ -56,7 +56,7 @@ public class MainActivity extends AppCompatActivity
 
         senSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         senAccelerometer = senSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        senSensorManager.registerListener((SensorEventListener) sel, senAccelerometer , SensorManager.SENSOR_DELAY_NORMAL);
+        senSensorManager.registerListener(sel, senAccelerometer , SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     @Override
